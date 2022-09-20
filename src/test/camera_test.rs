8963f29c4a4d1b56b9380fb4551e7d9a0babd686
chen <15305067103@163.com>
2022-09-20 17:47:47 +0800

    球的相交

---
+++ src/test/camera_test.rs
@@ -0,0 +1,16 @@
+mod test {
+    use crate::{
+        core::{
+            ray::Ray,
+            shape::{sphere::Sphere, BaseShapeAble},
+            *,
+        },
+        extends::{Mat4, Point3, Vec3},
+    };
+    use std::f32::consts::PI;
+
+    #[test]
+    fn test_camera() {
+        
+    }
+}
